f277ca7c95ad006ed82a7455339c178013fc5b2f
muyang00 <280798744@qq.com>
2017-02-15 16:14:18 +0800

    创建主根类

---
+++ YEDouYuZB/YEDouYuZB/Main/Controller/BaseViewController.swift
@@ -0,0 +1,61 @@
+//
+//  BaseViewController.swift
+//  YEDouYuZB
+//
+//  Created by yongen on 17/2/15.
+//  Copyright © 2017年 yongen. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    //MARK:- 定义属性
+    var contentView : UIView?
+    //MARK:- 懒加载属性
+    fileprivate lazy var animImageView : UIImageView = { [unowned self] in
+        let imageView = UIImageView(image: UIImage(named: "img_loading_1"))
+        imageView.center = self.view.center
+        imageView.animationImages = [UIImage(named: "img_loading_1")!, UIImage(named: "img_loading_2")!]
+        imageView.animationDuration = 0.5
+        imageView.animationRepeatCount = LONG_MAX
+        imageView.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin]
+        
+        return imageView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+
+}
+
+extension BaseViewController {
+    func setupUI() {
+        
+        //1.
+        contentView?.isHidden = true
+        
+        //2. 添加执行动画的UIImageView
+        view.addSubview(animImageView)
+        
+        //3. 
+        animImageView.startAnimating()
+        
+        //4. 
+        view.backgroundColor = UIColor (r: 250, g: 250, b: 250)
+    }
+    
+    func loadDataFinished() {
+        //1.
+        animImageView.stopAnimating()
+        //2. 
+        animImageView.isHidden = true
+        //3.
+        contentView?.isHidden = false
+    }
+}
+
---
+++ YEDouYuZB/YEDouYuZB/Main/Controller/CustomNavigationController.swift
@@ -0,0 +1,57 @@
+//
+//  CustomNavigationController.swift
+//  YEDouYuZB
+//
+//  Created by yongen on 17/2/15.
+//  Copyright © 2017年 yongen. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //1.
+        guard let systemGes = interactivePopGestureRecognizer else { return }
+        
+        //2.
+        guard let gesView = systemGes.view else {return}
+        
+        //3.获取target/action
+//        var count : UInt32 = 0
+//        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+//        for i in 0..<count {
+//            let ivar = ivars[Int(i)]
+//            let name = ivar_getName(ivar)
+//            print(String(cString: name!))
+//        }
+        
+        let targets = systemGes.value(forKey: "_targets") as? [NSObject]
+        guard let targetObjc = targets?.first else {
+            return
+        }
+        
+        //3.2 取出target
+        guard let target = targetObjc.value(forKey: "target") else {
+            return
+        }
+        //3.3取出Action
+        let action = Selector(("handleNavigationTransition:"))
+        //4.
+        let panGes = UIPanGestureRecognizer()
+        gesView.addGestureRecognizer(panGes)
+        panGes.addTarget(target, action: action)
+        
+        
+  }
+    
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        viewController.hidesBottomBarWhenPushed = true
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+
+}
